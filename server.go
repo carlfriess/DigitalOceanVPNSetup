17e64030817197eb4c46163c25cfd9ee8f004d80
Carl Friess <carl.friess@me.com>
2017-05-06 21:50:35 +0200

    Now waits for install to finish
    A simple web server is launched at the end of the installation, which is polled by the setup app. As soon as it responds, the app knows the setup is complete and the webserver terminates.

--- server.go
+++ server.go
@@ -6,20 +6,32 @@ import (
 	"log"
 	"os"
 	"time"
+	"github.com/rs/cors"
 )
 
 func exit() {
+
 	time.Sleep(time.Second)
 	os.Exit(0)
+	
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
+
 	w.Header().Add("content-type", "application/json")
 	io.WriteString(w, "{\"success\":true}")
 	go exit()
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	// Support CORS:
+	corsHandler := cors.Default().Handler(mux)
+
+	log.Fatal(http.ListenAndServe(":80", corsHandler))
+
 }
